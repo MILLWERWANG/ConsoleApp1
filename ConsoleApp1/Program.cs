61c9c2670d0e1a5bca6b4229ec259f32830d5216
millerwang <miller680523.public@gmail.com>
2020-04-19 13:24:58 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TEST
+{
+    public class CAR
+    {
+        public void run(string AA)
+        {
+            Console.WriteLine(AA + "開車囉");
+
+        }
+    }
+}
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TEST.CAR C = new TEST.CAR();
+            C.run("miller");
+
+        }
+    }
+}
