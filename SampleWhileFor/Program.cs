61a33f6402c6a48ab2ead9784133e4c7b53752c3
millerwang <miller680523.public@gmail.com>
2020-04-19 14:59:36 +0800

    While範例：小於20可被３整除的數字

---
+++ SampleWhileFor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//While範例：小於20可被３整除的數字
+namespace SampleWhileFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            StringBuilder aa = new StringBuilder();
+            while(i <21)
+                {
+                
+                if ((i%3)==0)
+                {
+                    aa.Append(i);
+                    aa.Append(",");
+                    i++;
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+               
+            }
+            Console.WriteLine($"可以被3整除的數字{aa}");
+
+            Console.ReadLine();
+        }
+    }
+}
